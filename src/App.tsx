cedcac25e66efde7f249d4236fd60216dfbaef41
Daniel Wolker <danewolker@gmail.com>
2026-04-29 20:10:00 -0300

    Refactor: migração completa para React e correção de deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import Home from './pages/Home';
 import ProductPage from './pages/ProductPage';
